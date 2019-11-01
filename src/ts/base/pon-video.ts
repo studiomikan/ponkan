6c3fb895315e20e893618dd0d609ef18b27d71aa
okayu <moka1988jp@gmail.com>
2019-11-02 01:32:33 +0900

    video関係の実装

---
+++ src/ts/base/pon-video.ts
@@ -0,0 +1,92 @@
+import * as PIXI from "pixi.js";
+
+/**
+ * PonVideoのコールバック
+ */
+export interface IPonVideoCallbacks {
+  /**
+   * コンテナに動画を追加する
+   * @param child 追加するスプライト
+   */
+  pixiContainerAddChild(child: PIXI.DisplayObject): void;
+  /**
+   * コンテナから動画を削除する
+   * @param child 削除するスプライト
+   */
+  pixiContainerRemoveChild(child: PIXI.DisplayObject): void;
+
+  /**
+   * 再生終了時
+   */
+  // onComplete(): void;
+}
+
+
+/**
+ * 動画スプライト
+ */
+export class PonVideo {
+
+  protected callbacks: IPonVideoCallbacks;
+  protected videoTexture: PIXI.Texture;
+  protected videoSprite: PIXI.Sprite;
+
+  public get source(): HTMLVideoElement { return this.videoTexture.baseTexture.source as HTMLVideoElement; }
+
+  public get width(): number { return this.videoSprite.width; }
+  public set width(width: number) { this.videoSprite.width = width; }
+  public get height(): number { return this.videoSprite.height; }
+  public set height(height: number) { this.videoSprite.height = height; }
+
+  public get loop(): boolean { return this.source.loop; }
+  public set loop(loop: boolean) { this.source.loop = loop; }
+
+  public get volume(): number { return this.source.volume; }
+  public set volume(volume: number) { this.source.volume = volume; }
+
+  public get playing(): boolean { return !this.source.ended; }
+
+  /**
+   * @param callbacks コールバック
+   */
+  public constructor(videoTexture: PIXI.Texture, callbacks: IPonVideoCallbacks) {
+    this.callbacks = callbacks;
+    this.videoTexture = videoTexture;
+    this.source.preload = "auto"; // for PIXI.js bug: https://github.com/pixijs/pixi.js/issues/5996
+    this.videoSprite = new PIXI.Sprite(this.videoTexture);
+    this.videoSprite.width = 700;
+    this.videoSprite.height = 700;
+    this.callbacks.pixiContainerAddChild(this.videoSprite);
+  }
+
+  /**
+   * 破棄
+   */
+  public destroy(): void {
+    this.clear();
+  }
+
+  /**
+   * スプライトをクリアする。
+   * 内部で保持しているPIXIのスプライトを開放する。
+   * このスプライトの保持している座標、サイズ、表示状態などはクリアされず、
+   * そのままの状態を保つ。
+   */
+  public clear(): void {
+    this.callbacks.pixiContainerRemoveChild(this.videoSprite);
+    this.videoSprite.destroy();
+  }
+
+  public play(): void {
+    this.source.play();
+  }
+
+  public stop(): void {
+    this.source.pause();
+    this.source.currentTime = 0;
+  }
+
+  public pause(): void {
+    this.source.pause();
+  }
+}
--- src/ts/tag-action.ts
+++ src/ts/tag-action.ts
@@ -2726,6 +2726,168 @@ export function generateTagActions(p: Ponkan3): TagAction[] {
       },
     ),
     // ======================================================================
+    // 動画再生関係
+    // ======================================================================
+    /// @category 動画再生
+    /// @description 動画を再生する
+    /// @details
+    ///   指定のレイヤーに動画ファイルを読み込み、再生します。
+    ///   動画の再生はストリーミング方式で行われます。\n
+    ///   ネットワーク回線によっては、動画がスムーズに再生できない可能性があります。
+    ///
+    ///   動画のサイズ（幅・高さ）は無視され、読み込んだレイヤーサイズに合わせて
+    ///   拡大・縮小されます。
+    ///
+    ///   対応する動画ファイル形式は、プレイするブラウザに依存します。
+    new TagAction(
+      ["loadvideo", "video"],
+      [
+        /// @param 対象レイヤー
+        new TagValue("lay", "string", true, null),
+        /// @param 対象ページ
+        new TagValue("page", "string", false, "current"),
+        /// @param 動画ファイル
+        new TagValue("file", "string", true, null),
+        /// @param 動画の幅(px)
+        new TagValue("width", "number", true, null),
+        /// @param 動画の高さ(px)
+        new TagValue("height", "number", true, null),
+        /// @param 読み込み完了後に自動的に再生するかどうか
+        new TagValue("autoplay", "boolean", false, true),
+        /// @param ループ再生するかどうか
+        new TagValue("loop", "boolean", false, false),
+      ],
+      (values: any, tick: number):  "continue" | "break" => {
+        const task: AsyncTask = new AsyncTask();
+        p.getLayers(values).forEach((layer) => {
+          task.add(() => {
+            return layer.loadVideo(values.file, values.width, values.height, values.autoplay, values.loop);
+          });
+        });
+        task.run().done(() => {
+          p.conductor.start();
+        }).fail(() => {
+          if (p.config && p.config.developMode) {
+            p.error(new Error(`動画読み込みに失敗しました。(${values.file})`));
+          } else {
+            p.error(new Error(`動画読み込みに失敗しました。`));
+          }
+        });
+        return p.conductor.stop()
+      },
+    ),
+    /// @category 動画再生
+    /// @description 動画を再生する
+    /// @details
+    ///   動画再生を開始します。
+    new TagAction(
+      ["playvideo"],
+      [
+        /// @param 対象レイヤー
+        new TagValue("lay", "string", true, null),
+        /// @param 対象ページ
+        new TagValue("page", "string", false, "current"),
+        /// @param ループ再生するかどうか
+        new TagValue("loop", "boolean", false, false),
+      ],
+      (values: any, tick: number):  "continue" | "break" => {
+        p.getLayers(values).forEach((layer) => {
+          layer.playVideo(values.loop);
+        });
+        return "continue";
+      },
+    ),
+    /// @category 動画再生
+    /// @description 動画を一時停止する
+    /// @details
+    ///   動画再生を一時停止します。
+    ///   再生再開するにはplayvideoコマンドを使用します。
+    new TagAction(
+      ["pausevideo"],
+      [
+        /// @param 対象レイヤー
+        new TagValue("lay", "string", true, null),
+        /// @param 対象ページ
+        new TagValue("page", "string", false, "current"),
+      ],
+      (values: any, tick: number):  "continue" | "break" => {
+        p.getLayers(values).forEach((layer) => {
+          layer.pauseVideo();
+        });
+        return "continue";
+      },
+    ),
+    /// @category 動画再生
+    /// @description 動画を停止する
+    /// @details
+    ///   動画再生を終了します。
+    new TagAction(
+      ["stopvideo"],
+      [
+        /// @param 対象レイヤー
+        new TagValue("lay", "string", true, null),
+        /// @param 対象ページ
+        new TagValue("page", "string", false, "current"),
+      ],
+      (values: any, tick: number):  "continue" | "break" => {
+        p.getLayers(values).forEach((layer) => {
+          layer.stopVideo();
+        });
+        return "continue";
+      },
+    ),
+    /// @category 動画再生
+    /// @description 動画を解放する
+    /// @details
+    ///   動画を解放します。
+    ///   動画再生が完了したら、必ずこのコマンドで解放するようにしてください。
+    new TagAction(
+      ["freevideo"],
+      [
+        /// @param 対象レイヤー
+        new TagValue("lay", "string", true, null),
+        /// @param 対象ページ
+        new TagValue("page", "string", false, "current"),
+      ],
+      (values: any, tick: number):  "continue" | "break" => {
+        p.getLayers(values).forEach((layer) => {
+          layer.freeVideo();
+        });
+        return "continue";
+      },
+    ),
+    /// @category 動画再生
+    /// @description 動画再生の終了を待つ
+    /// @details
+    ///   動画再生の終了を待ちます。\n
+    ///   動画再生中のレイヤーが無い場合やループ再生の場合はなにもしません。
+    new TagAction(
+      ["waitvideo", "wv"],
+      [
+        /// @param スキップ可能かどうか
+        new TagValue("canskip", "boolean", false, true),
+      ],
+      (values: any, tick: number):  "continue" | "break" => {
+        if (!p.hasPlayingVideoLayer) {
+          return "continue";
+        }
+        if (p.isSkipping && values.canskip) {
+          p.waitVideoClickCallback();
+          return "break";
+        } else {
+          if (values.canskip) {
+            p.conductor.addEventHandler(new PonEventHandler("click", (): void => {
+              p.waitVideoClickCallback();
+            }, "waitvideo"));
+          }
+          p.conductor.addEventHandler(new PonEventHandler("video", (): void => {
+            p.waitVideoCompleteCallback();
+          }, "waitvideo"));
+          return p.conductor.stop();
+        }
+      },
+    ),
+    // ======================================================================
     // セーブ＆ロード関係
     // ======================================================================
     /// @category セーブ／ロード
@@ -2766,7 +2928,7 @@ export function generateTagActions(p: Ponkan3): TagAction[] {
     /// @category セーブ／ロード
     /// @description 一時セーブする
     /// @details
-    ///   一時領域に、このコマンドを実行したときの状態を保存します。\n
+    ///   一時領域に、このコマンドを実行したときの状態を保存します。
     ///
     ///   ここで保存したセーブデータは通常のセーブデータとは別に保持されます。
     ///   また、あくまで一時領域に保存するだけなので、ゲームが終了するときに破棄されます。
