85b259ba796ef2533ed55b707818cb7d7d7fc6c1
okayu <moka1988jp@gmail.com>
2020-02-17 19:49:40 +0900

    testのd.tsが出力されないように

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -33,7 +33,7 @@ module.exports = merge(common, {
       indent: '  ',
       removeSource: true,
       newline: '\n',
-      exclude: /node_modules/
+      exclude: /node_modules|test/
     })
   ],
 });
